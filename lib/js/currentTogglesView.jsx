33fe3079cd8bbc233f0a686d655bf4c9a6e47ba7
nima.shahri <nima.shahri@auror.co>
2016-11-23 17:13:11 +1300

    some changes

--- lib/js/currentTogglesView.jsx
+++ lib/js/currentTogglesView.jsx
@@ -7,7 +7,7 @@ class CurrentTogglesView extends React.Component {
         let features = this.props.releaseToggleContext.features;
 
         return (
-            <div>
+            <div style="left: 168px; top: 0px; position: absolute; background-color: rgb(238, 238, 238); box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 10px; border: 1px solid rgb(204, 204, 204); border-radius: 3px; margin-left: -5px; margin-top: 5px; padding: 10px;">
                 {Object.keys(features).map(key => <div key={key}>{key}:{features[key].toString() }</div>)}
             </div>
         );
